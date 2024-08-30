994d33df11f21118ccdc10d5b8bcbfba08dbb8af
unknown <mahtok98212@gmail.com>
2024-08-30 09:25:55 +0530

    add the active link color and change the font

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FaUser } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 interface LayoutProps {
@@ -11,16 +11,16 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen bg-bgOne flex flex-col">
       <nav className="flex items-center bg-bgOne p-4 justify-between md:text-lg border-b-black border-b-[1px] fixed top-0 z-50 w-full">
         <div>
-          <Link to={"/home"}>VideoShare</Link>
+          <NavLink to={"/home"}>VideoShare</NavLink>
         </div>
         <div className="flex items-center gap-3 md:gap-5 lg:gap-7 capitalize">
-          <Link to={"/search-video"}>
+          <NavLink to={"/search-video"}>
             <FaSearch />
-          </Link>
-          <Link to={"/user/upload-video"}>upload</Link>
-          <Link to={"/user/profile"}>
+          </NavLink>
+          <NavLink to={"/user/upload-video"}>upload</NavLink>
+          <NavLink to={"/user/profile"}>
             <FaUser className="" />
-          </Link>
+          </NavLink>
         </div>
       </nav>
       <main className="flex-1 flex flex-col items-center w-full mt-16">
