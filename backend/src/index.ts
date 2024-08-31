dd305f32a8766ae2f7d832f4390e8966f91bba5d
unknown <mahtok98212@gmail.com>
2024-08-31 09:32:33 +0530

    add the api to avoid cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 import routes from "./routes";
 import passportJwtStrategy from "./config/passwordJwtStrategy";
-
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8080;
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://video-sharee-api.vercel.app/"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 connectDB();
 app.use(passportJwtStrategy.initialize());
