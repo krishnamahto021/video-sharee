319e519b0668cd93b7a056f47916bc3a543bdcb6
unknown <mahtok98212@gmail.com>
2024-09-04 08:54:19 +0530

    modify the user dashboard

--- frontend/src/pages/user/Dashboard.tsx
+++ frontend/src/pages/user/Dashboard.tsx
@@ -3,19 +3,55 @@ import Layout from "../../components/Layout";
 import Sidebar from "../../components/Sidebar";
 import { selectLoggedInUser } from "../../redux/reducers/auth/authReducer";
 import { useSelector } from "react-redux";
+import { FaUser, FaEnvelope, FaUserTag } from "react-icons/fa";
 
 const Dashboard: React.FC = () => {
   const loggedInUser = useSelector(selectLoggedInUser);
 
   return (
     <Layout>
-      <div className="flex w-full gap-2 pr-2 h-full">
+      <div className="flex w-full gap-2 pr-2 h-full m-2">
         <Sidebar />
         <main className="flex-1 p-4 mt-7 bg-white shadow-lg rounded-lg ml-0 md:ml-64">
           <h1 className="text-center font-semibold text-xl text-gray-700 mb-5">
             Dashboard
           </h1>
           <div className="container flex flex-col gap-6">
+            <div className="bg-gray-100 p-6 rounded-lg shadow-md mb-10">
+              <h2 className="text-xl font-bold mb-4">User Information</h2>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+                  <div className="bg-blue-100 p-2 rounded-full mr-4">
+                    <FaUser className="h-6 w-6 text-blue-500" />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold mb-1">Name</h3>
+                    <p className="text-gray-700 capitalize">
+                      {loggedInUser?.name}
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+                  <div className="bg-green-100 p-2 rounded-full mr-4">
+                    <FaEnvelope className="h-6 w-6 text-green-500" />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold mb-1">Email</h3>
+                    <p className="text-gray-700">{loggedInUser?.email}</p>
+                  </div>
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
+                  <div className="bg-red-100 p-2 rounded-full mr-4">
+                    <FaUserTag className="h-6 w-6 text-red-500" />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold mb-1">Role</h3>
+                    <p className="text-gray-700">user</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div className="flex flex-col lg:flex-row gap-3 justify-around items-center">
               <div className="bg-blue-100 p-6 rounded-lg shadow-md w-full lg:w-1/2">
                 <h2 className="text-lg font-semibold text-blue-600 mb-2">
