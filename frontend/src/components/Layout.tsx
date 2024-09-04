2ebfe762e11d5cc019ee0d0a2dd33613f2b19b57
unknown <mahtok98212@gmail.com>
2024-09-04 10:03:06 +0530

    remove footer from user pages

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
@@ -13,6 +13,11 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const currentYear = new Date().getFullYear();
   const loggedInUser = useSelector(selectLoggedInUser);
+  const location = useLocation();
+
+  // Check if the current route contains '/user'
+  const isUserRoute = location.pathname.includes("/user");
+
   return (
     <div className="min-h-screen bg-bgTwo flex flex-col">
       <nav className="flex items-center bg-bgFive p-4 justify-between md:text-lg border-b-black border-b-[1px] fixed top-0 z-50 w-full text-white">
@@ -27,7 +32,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <FaSearch />
           </NavLink>
           {loggedInUser?.token ? (
-            <NavLink to={"/user/dashboard"}> Dashobard</NavLink>
+            <NavLink to={"/user/dashboard"}>Dashboard</NavLink>
           ) : (
             <NavLink to={"/sign-in"}>Sign In</NavLink>
           )}
@@ -38,40 +43,42 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         {children}
       </main>
 
-      <footer className="bg-black text-center py-6 border-t-[1px] border-t-black z-50">
-        <div className="flex justify-center gap-6 mb-4 text-white">
-          <a
-            href="https://github.com/krishnamahto021"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-          >
-            <FaGithub size={24} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/krishnamahto021"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedin size={24} />
-          </a>
-          <a
-            href="https://twitter.com/krishnamahto021"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Twitter"
-          >
-            <FaTwitter size={24} />
-          </a>
-        </div>
-        <p className="text-sm text-gray-400 mb-2">
-          Sharing the joy of videos with the world.
-        </p>
-        <p className="text-sm text-gray-400">
-          Copyright © {currentYear} VideoShare. All rights reserved.
-        </p>
-      </footer>
+      {!isUserRoute && (
+        <footer className="bg-black text-center py-6 border-t-[1px] border-t-black z-50">
+          <div className="flex justify-center gap-6 mb-4 text-white">
+            <a
+              href="https://github.com/krishnamahto021"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/krishnamahto021"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin size={24} />
+            </a>
+            <a
+              href="https://twitter.com/krishnamahto021"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
+              <FaTwitter size={24} />
+            </a>
+          </div>
+          <p className="text-sm text-gray-400 mb-2">
+            Sharing the joy of videos with the world.
+          </p>
+          <p className="text-sm text-gray-400">
+            Copyright © {currentYear} VideoShare. All rights reserved.
+          </p>
+        </footer>
+      )}
     </div>
   );
 };
